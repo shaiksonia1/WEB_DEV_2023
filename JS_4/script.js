3d6f246dea7de197f773edb6a82ecec417d12dd5
Sonia <shaiksonia222@gmail.com>
2024-02-06 12:40:09 +0530

    JS_basics

--- JS_4/script.js
+++ JS_4/script.js
@@ -15,12 +15,44 @@
 // let avg = sum/marks.length;
 
 
-function myfunction (){
-    console.log("sonia")
-}
+// function myfunction (){
+//     console.log("sonia")
+// }
 
-myfunction(x,y);
-function sum(x,y){
-console.log(x+y);
-}
+// myfunction(x,y);
+// function sum(x,y){
+// console.log(x+y);
+// }
+
+// arrow functions
+
+// const arrowmulti = (a,b)=>{
+// return a*b;
+// }
+
+// function find vowels in a string
+// function countVowels (str){
+//     let count=0;
+//     for(const char of str ){
+//         if(char=== "a"||char === "e" || char === "i" || char === "o" || char==="u"){
+//             count++;
+//         }
+//     }
+// return count;
+// }
+
+// const countVow = (str) =>{
+//     let count=0;
+//     for(const char of str ){
+//         if(char=== "a"||char === "e" || char === "i" || char === "o" || char==="u"){
+//             count++;
+//         }
+//     }
+// }
+
+// let num =[1,2,3,4,5];
+ 
+// num.forEach((num) => {
+//     console.log(num*num);
+// });
 
