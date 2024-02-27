b0a980b22fc2ad21b3309d405ce4890e96145206
Sonia <shaiksonia222@gmail.com>
2024-02-27 18:23:02 +0530

    DOM Manipulations (Properties and tags)

---
+++ JS_5/script.js
@@ -0,0 +1,28 @@
+let heading = document.getElementById("heading");
+console.log(heading)
+
+let subhead = document.getElementsByClassName("sub-head")
+console.log(subhead);
+console.dir(subhead)
+
+let firstelements = document.querySelector("p");
+console.dir(firstelements)
+
+// // this document.queryselctor means it will select only first element
+// // this document.queryselctorall means it will select only all elements
+// // this is for tags
+
+let allelements = document.querySelectorAll("li");
+console.dir(allelements);
+
+//this one is for clasess we should add "." to each class to access elements
+
+let classes =  document.querySelector(".sub-head");
+console.dir(sub-head);
+
+// this one is for id we have to use # for id to access elementss
+let container =  document.querySelector("#container");
+console.dir(container)
+
+document.querySelector("div")
+document.querySelector("div").children;
---
+++ JS_6/script.js
@@ -0,0 +1,16 @@
+// let firstelements = document.querySelector("#container")
+// console.dir(container)
+
+
+// let div =  document.querySelector("div")
+// console.dir(div)
+
+// let h2 = document.querySelector("h2")
+// console.dir(h2)
+
+// h2.innerText = h2.innerText +  " from apna college students"
+
+let divs = document.querySelectorAll(".box")
+console.log(divs)
+
+divs[1].innerText = "unique text";
